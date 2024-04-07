fc88b6fe93b07c98e48f2c6390f0f45ccdb3f6b5
ArthurKhalifa <arthursantosae@hotmail.com>
2024-04-07 11:17:04 -0300

    deploy configurações

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite-Atual-Portfolio",
+  base: "/Atual-Portfolio",
   plugins: [react()],
 })
