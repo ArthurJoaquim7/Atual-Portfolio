458d79cbe1d29be8963372ec7f79a7f8c8196dae
ArthurKhalifa <arthursantosae@hotmail.com>
2024-04-09 10:42:03 -0300

    Fix bug pages

--- src/Components/Parallax/Parallax.jsx
+++ src/Components/Parallax/Parallax.jsx
@@ -1,7 +1,7 @@
 import style from '../Parallax/Parallax.module.css'
 import waveTest from '../Img/waveTest.png'
+import codeF from '../Img/codeF.png'
 import vaca from '../Img/vaca.png'
-import vacaCode from '../img/vacaCode.png'
 import emailZoom from '../img/emailZoom.png'
 import { motion, useScroll, useTransform } from 'framer-motion';
 import { useRef } from 'react';
@@ -19,7 +19,7 @@ export const Parallax = () => {
         <motion.div className={style.container} ref={ref}>
             <div className={style.floating}>
                 <img src={vaca} className={style.vaca} />
-                <img src={vacaCode} className={style.vacaCode} />
+                <img src={codeF} className={style.vacaCode} />
             </div>
             <img src={waveTest} className={style.waveBottom} />
             <div className={style.centered}>
