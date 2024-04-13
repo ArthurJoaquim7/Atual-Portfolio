4aa200895f07945e8db6894e513f8b8a73e78d7d
ArthurKhalifa <arthursantosae@hotmail.com>
2024-04-13 11:24:01 -0300

    Add smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react'
 import './App.css'
 import { Header } from './Components/Header/Header'
-
+import Lenis from '@studio-freight/lenis'
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis()
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
 
+    requestAnimationFrame(raf)
+  }, [])
   return (
     <>
       <div className="App">
